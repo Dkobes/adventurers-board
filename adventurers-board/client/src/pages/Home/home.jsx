a8c7a61def67921b34b3b2a1a5bb94bdac33cfdd
Danyelle <dkobes15@gmail.com>
2025-01-03 20:11:45 -0600

    Home background

--- adventurers-board/client/src/pages/Home/home.jsx
+++ adventurers-board/client/src/pages/Home/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './home.css';
 
 const Home = () => {
     return ( 
-        <div>
-            <h1>Home</h1>
+        <div className="home">
+            <h1>Adventurer's Board</h1>
         </div>   
      );
 }
