7808edf86005d1a276e4a58295b2e77cc85109cc
Danyelle <dkobes15@gmail.com>
2025-01-07 20:47:03 -0600

    Notes CSS

--- adventurers-board/client/src/pages/Notes/notes.jsx
+++ adventurers-board/client/src/pages/Notes/notes.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import NotePaper from '/src/assets/images/note-paper.jpg';
+import './notes.css';
 
 const Notes = () => {
     const [notes, setNotes] = useState([]);
@@ -17,26 +18,33 @@ const Notes = () => {
     };
 
     return (
-        <div className="note-paper">
-            <img src={NotePaper} alt="Stained note paper" className="note-paper"></img>
-            <h2>Notes</h2>
-            <textarea 
-                value={noteText} 
-                onChange={(e) => setNoteText(e.target.value)} 
-                placeholder="Write your note here" 
-            />
-            <button onClick={addNote}>Add Note</button>
-            <ul>
-                {notes.map((note, index) => (
-                    <li key={index}>{note}
-                        <button onClick={() => removeNote(index)} style={{ marginLeft: '10px' }}>
-                            Remove
-                        </button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+        <div>
+            <div className='note-paper'>
+                <img src={NotePaper} alt="Stained note paper" className='notepaper-img'></img>
+            </div>
+            <div className='text-overlay'>
+                <textarea 
+                    value={noteText} 
+                    onChange={(e) => setNoteText(e.target.value)} 
+                    placeholder="Write your note here..." 
+                />
+            </div>
+            <div className='button-overlay'>
+                <button onClick={addNote}>Add Note</button>
+                    <ul>
+                        <div class="list">
+                        {notes.map((note, index) => (
+                            <li key={index}>{note}
+                                <button onClick={() => removeNote(index)} style={{ marginLeft: '10px' }}>
+                                Remove
+                                </button>
+                            </li>
+                        ))}
+                        </div>
+                    </ul>
+            </div>
+         </div>
+     );
 };
 
 export default Notes;
